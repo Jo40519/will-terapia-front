fb0be59331d58a8c89b972edaf02df220cd4bcdb
Jo40519 <jaolobato85@gmail.com>
2025-02-10 16:30:29 -0300

    update angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,5 +34,18 @@ export class AppComponent implements OnInit {
     this.translateService.get('primeng').subscribe(res => this.primengConfig.setTranslation(res));
 }
 
+rotaFireWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso:', registration);
+      })
+      .catch((error) => {
+        console.error('Falha ao registrar o Service Worker:', error);
+      });
+  }
+}
+
 
 }
